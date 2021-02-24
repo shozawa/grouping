4016ea4913c5f5fac281d7fcee6bb7b1537f4b96
shozawa <tomohiro.shozawa@gmail.com>
2021-02-24 09:12:17 +0900

    基本機能を実装

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import { times, shuffle } from "lodash";
+
+function chunk(array, numGroups) {
+  const dist = times(numGroups, () => []);
+  array.forEach((elm, index) => {
+    dist[index % numGroups].push(elm);
+  });
+  return dist;
+}
+
+function isWhitespace(text) {
+  return /^\s*$/.test(text);
+}
+
+function parseCSV(string) {
+  return string
+    .split("\n")
+    .filter((row) => !isWhitespace(row))
+    .map((row) => row.split(","));
+}
+
+function toCSV(matrix) {
+  return matrix.map((row) => row.join(",")).join("\n");
+}
+
+function group(members, numGroups) {
+  const copy = members.slice();
+  copy.sort((a, b) => a.relationship.localeCompare(b.relationship));
+
+  return chunk(copy, numGroups);
+}
+
+function groupsToCSV(groups) {
+  const matrix = groups.flatMap((g, i) =>
+    g.map((m) => [`グループ${i + 1}`, m.name])
+  );
+  return toCSV(matrix);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("submit");
+  const textarea = document.getElementById("input");
+  const output = document.getElementById("output");
+  const numGroups = document.getElementById("num-groups");
+
+  button.addEventListener("click", () => {
+    const csv = parseCSV(textarea.value);
+
+    const members = csv.map((row) => {
+      const [name, relationship] = row;
+      return { name, relationship };
+    });
+
+    const groups = group(shuffle(members), numGroups.value);
+
+    output.value = groupsToCSV(groups);
+  });
+});
